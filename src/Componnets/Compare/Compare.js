87cd790e5b05900deee0fa8682490f6c606df51e
AhshanHabib26 <habib.net27@gmail.com>
2022-07-22 23:36:28 +0600

    Initial Setup

---
+++ src/Componnets/Compare/Compare.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Styles from '../Compare/Compare.module.css'
+
+const Compare = () => {
+    return (
+        <div>
+           <div className={Styles.textBoxs}>
+            <input name='inputA' type="text" />
+            <input name='inputB' type="text" />
+            </div> 
+            <button type="submit">Compute</button>
+        </div>
+    );
+};
+
+export default Compare;
