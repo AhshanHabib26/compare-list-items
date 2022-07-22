5d8c5cce3a23d3e0b3d567df2713603a4c252f80
AhshanHabib26 <habib.net27@gmail.com>
2022-07-22 23:38:22 +0600

    Copare Componnet Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { ToastContainer } from 'react-toastify';
+import Compare from './Componnets/Compare/Compare';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Compare Two List Items </h1>
+      <Compare/>
       <ToastContainer/>
     </div>
   );
